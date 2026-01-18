c3db2fcab5fbdbe71e6635c14efc5bcb8dea7ed8
Anton Swarts <antons@interfile.co.za>
2026-01-18 18:23:30 +0200

    added security config for cors

--- backEnd/eduthriftBackend/src/main/java/za/co/thrift/eduthrift/config/CorsConfig.java
+++ backEnd/eduthriftBackend/src/main/java/za/co/thrift/eduthrift/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package your.package.config;
+package za.co.thrift.eduthrift.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
--- backEnd/eduthriftBackend/src/main/java/za/co/thrift/eduthrift/config/SecurityConfig.java
+++ backEnd/eduthriftBackend/src/main/java/za/co/thrift/eduthrift/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package your.package.config;
+package za.co.thrift.eduthrift.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
