aff7801547f505dc5a9be9c11f4f7e68514b6e31
Anton Swarts <antons@interfile.co.za>
2026-02-04 21:13:51 +0200

    fixed admin args

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://154.65.107.50:8080', // backend API
+    baseURL: process.env.REACT_APP_API_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
